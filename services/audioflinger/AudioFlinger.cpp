6751d25361aeb5bcbbada902542cee296842fda1
Preetam Singh Ranawat <apranawat@codeaurora.org>
2013-10-03 13:36:47 +0530

    frameworks/av: port fixes in LPA effects
     -audio: fix for deadlock issue during LPA playback.
     -frameworks/av: Fix the LPA-AudioEffects dead lock issue CRs-Fixed: 477511
     -audioflinger: Fix pops in LPA while effects are applied CRs-Fixed: 526799
     -audioflinger: Fix delay in enabling effect for LPA CRs-Fixed: 525510
    
    Change-Id: I92118b5cba17f5337608007052e7dc891d2d6b5b
    (cherry picked from commit b24a38903f3fc2fd2bf7cc8fb71d50e1ff38162d)
    (cherry picked from commit e8a1d6f80e64822d83f04f4f0704890644048189)
    (cherry picked from commit 571f36890abe9f6b8bfe7341776cb5b3975bf952)

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -641,11 +641,11 @@ Exit:
 
 void AudioFlinger::deleteEffectSession()
 {
-    Mutex::Autolock _l(mLock);
     ALOGV("deleteSession");
     // -2 is invalid session ID
     mLPASessionId = -2;
     if (mLPAEffectChain != NULL) {
+        mLPAEffectChain->lock();
         mLPAEffectChain->setLPAFlag(false);
         size_t i, numEffects = mLPAEffectChain->getNumEffects();
         for(i = 0; i < numEffects; i++) {
@@ -658,6 +658,7 @@ void AudioFlinger::deleteEffectSession()
             }
             effect->configure();
         }
+        mLPAEffectChain->unlock();
         mLPAEffectChain.clear();
         mLPAEffectChain = NULL;
     }
